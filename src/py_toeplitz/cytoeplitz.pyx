75da0f58c9c4ad5c15fb3912927a25b0e87fb410
DWesl <22566757+DWesl@users.noreply.github.com>
2020-02-22 12:28:11 -0500

    Use SciPy's BLAS wrappers in cython toeplitz code.
    Can't speed up all loops, but can speed up some.

--- src/py_toeplitz/cytoeplitz.pyx
+++ src/py_toeplitz/cytoeplitz.pyx
@@ -8,7 +8,7 @@ cimport numpy as np
 from scipy.sparse.linalg.interface import LinearOperator
 # These require five arguments, and do not accept two.  I would need
 # to write a wrapper to fill in the details.
-# from scipy.linalg cimport cython_blas # sdot, ddot, cdotu, zdotu
+from scipy.linalg cimport cython_blas # sdot, ddot, cdotu, zdotu
 
 ctypedef fused numeric_type:
     np.float32_t
@@ -20,6 +20,39 @@ ctypedef fused numeric_type:
     np.int64_t
     np.complex64_t
     np.complex128_t
+
+
+ctypedef fused blas_types:
+    np.float32_t
+    np.float64_t
+    np.complex64_t
+    np.complex128_t
+
+
+cdef blas_types blas_dot(blas_types[::1] vec1, blas_types[::1] vec2):
+    """Compute BLAS-accelerated dot product.
+
+    Parameters
+    ----------
+    vec1, vec2: blas_types[::1]
+
+    Returns
+    -------
+    blas_types
+    """
+    cdef int n = len(vec1)
+    cdef int inc1 = 1
+    cdef int inc2 = 1
+    cdef blas_types result
+    if blas_types is np.float32_t:
+        result = cython_blas.sdot(&n, &vec1[0], &inc1, &vec2[0], &inc2)
+    elif blas_types is np.float64_t:
+        result = cython_blas.ddot(&n, &vec1[0], &inc1, &vec2[0], &inc2)
+    elif blas_types is np.complex64_t:
+        result = cython_blas.cdotu(&n, &vec1[0], &inc1, &vec2[0], &inc2)
+    elif blas_types is np.complex128_t:
+        result = cython_blas.zdotu(&n, &vec1[0], &inc1, &vec2[0], &inc2)
+    return result
 
 
 class CyToeplitz(LinearOperator):
@@ -90,13 +123,21 @@ class CyToeplitz(LinearOperator):
         cdef numeric_type[::1] data
         cdef numeric_type[:, ::1] result
         cdef numeric_type[::1] tmp
-        cy_dot = dot
+        cdef long int n_cols_vec = vec.shape[1]
         n_rows, n_columns = self.shape
         data = self._data
         n_data = len(data)
-        result = empty((n_rows, vec.shape[1]), dtype=self.dtype, order="C")
+        result = empty((n_rows, n_cols_vec), dtype=self.dtype, order="C")
         dot_start = n_rows - 1
-        for i in range(n_rows):
-            tmp = cy_dot(data[dot_start - i: n_data - i], vec)
-            result[i, :] = tmp
+        if numeric_type in blas_types:
+            for j in range(n_cols_vec):
+                tmp = vec[:, j].copy()
+                for i in range(n_rows):
+                    result[i, j] = blas_dot(data[dot_start - i: n_data - i], tmp)
+        else:
+            cy_dot = dot
+            for i in range(n_rows):
+                tmp = cy_dot(data[dot_start - i: n_data - i], vec)
+                result[i, :] = tmp
+
         return result
