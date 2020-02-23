0a9b6129ab1b5d01974960267c5eea4d7b9b3d8c
DWesl <22566757+DWesl@users.noreply.github.com>
2020-02-23 10:33:57 -0500

    Get FFTToeplitz operator working for real inputs.
    I can't figure out what's going wrong with the complex ones.

--- src/py_toeplitz/__init__.py
+++ src/py_toeplitz/__init__.py
@@ -1,4 +1,7 @@
-from numpy import dot, empty, newaxis
+from functools import partial
+
+from numpy import dot, empty, zeros, newaxis, around
+from numpy.fft import fft, ifft, rfft, irfft
 
 from scipy.sparse.linalg.interface import LinearOperator
 from scipy.signal import convolve
@@ -132,3 +135,82 @@ class ConvolveToeplitz(LinearOperator):
         if expand_dims:
             result = result[:, newaxis]
         return result
+
+
+class FFTToeplitz(LinearOperator):
+    """Toeplitz operator using FFT."""
+
+    def __init__(self, first_column, first_row=None):
+        """Construct a toeplitz operator.
+
+        Parameters
+        ----------
+        first_column : array_like
+            First column of the matrix
+        first_row : array_like, optional
+            First row of the matrix; first element must be same as
+            that of `first_column`
+
+        See Also
+        --------
+        scipy.linalg.toeplitz : Construct the full array
+        """
+        if first_row is None:
+            first_row = first_column
+        n_rows = len(first_column)
+        n_cols = len(first_row)
+        dtype = first_column.dtype
+        super(FFTToeplitz, self).__init__(
+            shape=(n_rows, n_cols),
+            dtype=dtype
+        )
+        computational_shape = n_rows + n_cols - 1
+        data = empty(computational_shape, dtype=dtype)
+        data[-n_cols:] = first_row
+        data[:n_rows] = first_column[::-1]
+        self._data = data
+
+        if dtype.kind == "c":
+            self._fft = partial(fft, n=computational_shape)
+            self._ifft = partial(ifft, n=computational_shape)
+        else:
+            self._fft = partial(rfft, n=computational_shape)
+            self._ifft = partial(irfft, n=computational_shape)
+        data = empty(computational_shape, dtype=dtype)
+        data[1:n_cols] = first_row[1:]
+        data[n_cols:] = first_column[:0:-1]
+        data[0] = first_column[0]
+        spectrum = self._fft(data)
+        self._spectrum = spectrum.conj()
+        # if self.dtype.kind == "c":
+        #     self._spectrum = spectrum
+        # else:
+        #     self._spectrum = spectrum.conj()
+
+    def _matvec(self, vec):
+        """Compute product of self with vec.
+
+        Parameters
+        ----------
+        vec: ndarray
+
+        Returns
+        -------
+        ndarray
+        """
+        n_rows, n_cols = self.shape
+        expand_dims = False
+        if vec.ndim == 2:
+            vec = vec[:, 0]
+            expand_dims = True
+        to_transform = zeros(n_rows + n_cols - 1, dtype=vec.dtype)
+        to_transform[:n_cols] = vec
+        vec_spec = self._fft(to_transform)
+        vec_spec *= self._spectrum
+        result = self._ifft(vec_spec)
+        result = result[:n_rows]
+        if expand_dims:
+            result = result[:, newaxis]
+        if self.dtype.kind != "i":
+            return result.astype(self.dtype)
+        return around(result).astype(self.dtype)
--- tests/test_operator.py
+++ tests/test_operator.py
@@ -8,7 +8,7 @@ from hypothesis.extra.numpy import (arrays, floating_dtypes, integer_dtypes,
 from hypothesis.strategies import (shared, integers, tuples, floats,
                                    sampled_from, complex_numbers)
 
-from py_toeplitz import Toeplitz, ConvolveToeplitz
+from py_toeplitz import Toeplitz, ConvolveToeplitz, FFTToeplitz
 from py_toeplitz.cytoeplitz import CyToeplitz
 
 MAX_ARRAY = 10
@@ -17,6 +17,9 @@ FLOAT_SIZES = (32, 64, 128)
 INTEGER_SIZES = (8, 16, 32, 64)
 COMPLEX_SIZES = (64, 128)
 INT8_MAX = 128
+OPERATOR_LIST = (Toeplitz, CyToeplitz,
+                 ConvolveToeplitz, FFTToeplitz)
+ATOL_MIN = 1e-14
 
 # @given(
 #     arrays(
@@ -42,12 +45,15 @@ INT8_MAX = 128
 @given(
     integers(min_value=1, max_value=MAX_ARRAY),
     integers(min_value=1, max_value=MAX_ARRAY),
-    sampled_from((Toeplitz, ConvolveToeplitz)),
+    sampled_from(OPERATOR_LIST),
 )
-def test_toeplitz_shape(n_rows, n_cols, toep_cls):
+def test_toeplitz_shape_dtype(n_rows, n_cols, toep_cls):
     first_col = np.empty(n_rows)
     first_row = np.empty(n_cols)
-    assert toeplitz(first_col, first_row).shape == toep_cls(first_col, first_row).shape
+    matrix = toeplitz(first_col, first_row)
+    operator = toep_cls(first_col, first_row)
+    assert matrix.shape == operator.shape
+    assert matrix.dtype == operator.dtype
 
 
 @given(
@@ -69,7 +75,7 @@ def test_toeplitz_shape(n_rows, n_cols, toep_cls):
         ),
         elements=floats(allow_infinity=False, allow_nan=False, width=32)
     ),
-    sampled_from((Toeplitz, ConvolveToeplitz, CyToeplitz)),
+    sampled_from(OPERATOR_LIST),
 )
 def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
     """Test toeplitz for real inputs."""
@@ -82,19 +88,22 @@ def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
     elif first_col.dtype == np.float64:
         atol_frac = 1e-14
     elif first_col.dtype == np.float128:
-        atol_frac = 1e-15
-    max_el = (
-        max(np.max(np.abs(first_col)), np.max(np.abs(first_row))) *
-        np.max(np.abs(test))
-    )
+        atol_frac = 1.1e-15
+    max_el = np.max(np.abs(first_col))
+    if len(first_row) > 1:
+        max_el = max(max_el, np.max(np.abs(first_row[1:])))
+    max_el *= np.max(np.abs(test))
     mat_result = full_mat.dot(test)
     if first_col.dtype == np.float32:
         # Apparently `np.dot` uses an extended-precision accumulator
         assume(np.all(np.isfinite(mat_result)))
+    op_result = toeplitz_op.dot(test)
+    if toep_cls == FFTToeplitz:
+        assume(np.all(np.isfinite(op_result)))
     np_tst.assert_allclose(
-        toeplitz_op.dot(test),
+        op_result,
         mat_result,
-        atol=atol_frac * max_el, rtol=atol_frac
+        atol=atol_frac * max_el + ATOL_MIN, rtol=atol_frac
     )
 
 
@@ -114,17 +123,28 @@ def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
             integers(min_value=1, max_value=MAX_ARRAY)
         ),
     ),
-    sampled_from((Toeplitz, ConvolveToeplitz, CyToeplitz)),
+    sampled_from(OPERATOR_LIST),
 )
 def test_toeplitz_int_mat(first_col, first_row, test, toep_cls):
     """Test toeplitz for real inputs."""
     full_mat = toeplitz(first_col, first_row)
     toeplitz_op = toep_cls(first_col, first_row)
     mat_result = full_mat.dot(test)
-    if toep_cls == ConvolveToeplitz:
+    if toep_cls in (ConvolveToeplitz, FFTToeplitz):
         rtol = 2e-6
     else:
         rtol = 0
+    if toep_cls == FFTToeplitz:
+        mat_result_long = toeplitz(
+            first_col.astype(float),
+            first_row.astype(float)
+        ).dot(
+            test.astype(float)
+        )
+        assume(np.allclose(
+            mat_result,
+            mat_result_long.astype(first_col.dtype)
+        ))
     np_tst.assert_allclose(
         toeplitz_op.dot(test),
         mat_result,
@@ -148,7 +168,7 @@ def test_toeplitz_int_mat(first_col, first_row, test, toep_cls):
             integers(min_value=1, max_value=MAX_ARRAY)
         ),
     ),
-    sampled_from((Toeplitz, ConvolveToeplitz, CyToeplitz)),
+    sampled_from(OPERATOR_LIST),
 )
 def test_toeplitz_complex_mat(first_col, first_row, test, toep_cls):
     """Test toeplitz for real inputs."""
@@ -160,10 +180,10 @@ def test_toeplitz_complex_mat(first_col, first_row, test, toep_cls):
         atol_frac = 1e-14
     elif first_col.dtype == np.complex256:
         atol_frac = 1e-15
-    max_el = (
-        max(np.max(np.abs(first_col)), np.max(np.abs(first_row))) *
-        np.max(np.abs(test))
-    )
+    max_el = np.max(np.abs(first_col))
+    if len(first_row) > 1:
+        max_el = max(max_el, np.max(np.abs(first_row[1:])))
+    max_el *= np.max(np.abs(test))
     mat_result = full_mat.dot(test)
     # Apparently `np.dot` uses an extended-precision accumulator
     assume(np.all(np.isfinite(mat_result)))
@@ -173,5 +193,5 @@ def test_toeplitz_complex_mat(first_col, first_row, test, toep_cls):
     np_tst.assert_allclose(
         op_result,
         mat_result,
-        atol=atol_frac * max_el, rtol=atol_frac
+        atol=atol_frac * max_el + ATOL_MIN, rtol=atol_frac
     )
