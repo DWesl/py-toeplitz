71307a43729904d64c3ec8bcc3f8d673201c5b6d
DWesl <22566757+DWesl@users.noreply.github.com>
2020-02-23 11:00:22 -0500

    Parameterize the tests over implementations.
    This lets hypothesis work separately on each implementation, giving it
    more time to find problems, and also lets the number of failing tests
    (1/12) better match the number of problems in the package, as compared
    to the previous 1/4.  The found failure is stull just
    `FFTToeplitz(complex)`, at least after fixing the other tests.

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.testing as np_tst
 from scipy.linalg import toeplitz
 
+import pytest
+
 from hypothesis import given, assume
 from hypothesis.extra.numpy import (arrays, floating_dtypes, integer_dtypes,
                                     complex_number_dtypes)
@@ -42,12 +44,12 @@ ATOL_MIN = 1e-14
 #     np_tst.assert_allclose(full_mat.dot(test), toeplitz_op.dot(test))
 
 
+@pytest.mark.parametrize("toep_cls", OPERATOR_LIST)
 @given(
     integers(min_value=1, max_value=MAX_ARRAY),
     integers(min_value=1, max_value=MAX_ARRAY),
-    sampled_from(OPERATOR_LIST),
 )
-def test_toeplitz_shape_dtype(n_rows, n_cols, toep_cls):
+def test_toeplitz_shape_dtype(toep_cls, n_rows, n_cols):
     first_col = np.empty(n_rows)
     first_row = np.empty(n_cols)
     matrix = toeplitz(first_col, first_row)
@@ -56,6 +58,7 @@ def test_toeplitz_shape_dtype(n_rows, n_cols, toep_cls):
     assert matrix.dtype == operator.dtype
 
 
+@pytest.mark.parametrize("toep_cls", OPERATOR_LIST)
 @given(
     arrays(
         shared(floating_dtypes(sizes=FLOAT_SIZES, endianness="="), key="dtype"),
@@ -75,9 +78,8 @@ def test_toeplitz_shape_dtype(n_rows, n_cols, toep_cls):
         ),
         elements=floats(allow_infinity=False, allow_nan=False, width=32)
     ),
-    sampled_from(OPERATOR_LIST),
 )
-def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
+def test_toeplitz_real_mat(toep_cls, first_col, first_row, test):
     """Test toeplitz for real inputs."""
     full_mat = toeplitz(first_col, first_row)
     toeplitz_op = toep_cls(first_col, first_row)
@@ -92,7 +94,8 @@ def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
     max_el = np.max(np.abs(first_col))
     if len(first_row) > 1:
         max_el = max(max_el, np.max(np.abs(first_row[1:])))
-    max_el *= np.max(np.abs(test))
+    if max_el != 0:
+        max_el *= np.max(np.abs(test))
     mat_result = full_mat.dot(test)
     if first_col.dtype == np.float32:
         # Apparently `np.dot` uses an extended-precision accumulator
@@ -103,10 +106,12 @@ def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
     np_tst.assert_allclose(
         op_result,
         mat_result,
-        atol=atol_frac * max_el + ATOL_MIN, rtol=atol_frac
+        atol=atol_frac * max_el + ATOL_MIN * len(test),
+        rtol=atol_frac
     )
 
 
+@pytest.mark.parametrize("toep_cls", OPERATOR_LIST)
 @given(
     arrays(
         shared(integer_dtypes(sizes=INTEGER_SIZES, endianness="="), key="dtype"),
@@ -123,18 +128,21 @@ def test_toeplitz_real_mat(first_col, first_row, test, toep_cls):
             integers(min_value=1, max_value=MAX_ARRAY)
         ),
     ),
-    sampled_from(OPERATOR_LIST),
 )
-def test_toeplitz_int_mat(first_col, first_row, test, toep_cls):
-    """Test toeplitz for real inputs."""
+def test_toeplitz_int_mat(toep_cls, first_col, first_row, test):
+    """Test toeplitz for integer inputs."""
     full_mat = toeplitz(first_col, first_row)
     toeplitz_op = toep_cls(first_col, first_row)
     mat_result = full_mat.dot(test)
     if toep_cls in (ConvolveToeplitz, FFTToeplitz):
         rtol = 2e-6
-    else:
-        rtol = 0
-    if toep_cls == FFTToeplitz:
+        max_el = np.max(np.abs(first_col))
+        if len(first_row) > 1:
+            max_el = max(max_el, np.max(np.abs(first_row[1:])))
+        # if max_el != 0:
+        #     max_el *= np.max(np.abs(test))
+        # assume(np.array(max_el, first_col.dtype)  == max_el)
+        atol = rtol * max_el * np.max(np.abs(test))
         mat_result_long = toeplitz(
             first_col.astype(float),
             first_row.astype(float)
@@ -145,13 +153,17 @@ def test_toeplitz_int_mat(first_col, first_row, test, toep_cls):
             mat_result,
             mat_result_long.astype(first_col.dtype)
         ))
+    else:
+        rtol = 0
+        atol = 0
     np_tst.assert_allclose(
         toeplitz_op.dot(test),
         mat_result,
-        rtol=rtol
+        rtol=rtol, atol=atol
     )
 
 
+@pytest.mark.parametrize("toep_cls", OPERATOR_LIST)
 @given(
     arrays(
         shared(complex_number_dtypes(sizes=COMPLEX_SIZES, endianness="="), key="dtype"),
@@ -168,10 +180,9 @@ def test_toeplitz_int_mat(first_col, first_row, test, toep_cls):
             integers(min_value=1, max_value=MAX_ARRAY)
         ),
     ),
-    sampled_from(OPERATOR_LIST),
 )
-def test_toeplitz_complex_mat(first_col, first_row, test, toep_cls):
-    """Test toeplitz for real inputs."""
+def test_toeplitz_complex_mat(toep_cls, first_col, first_row, test):
+    """Test toeplitz for complex inputs."""
     full_mat = toeplitz(first_col, first_row)
     toeplitz_op = toep_cls(first_col, first_row)
     if first_col.dtype == np.complex64:
@@ -183,7 +194,8 @@ def test_toeplitz_complex_mat(first_col, first_row, test, toep_cls):
     max_el = np.max(np.abs(first_col))
     if len(first_row) > 1:
         max_el = max(max_el, np.max(np.abs(first_row[1:])))
-    max_el *= np.max(np.abs(test))
+    if max_el != 0:
+        max_el *= np.max(np.abs(test))
     mat_result = full_mat.dot(test)
     # Apparently `np.dot` uses an extended-precision accumulator
     assume(np.all(np.isfinite(mat_result)))
